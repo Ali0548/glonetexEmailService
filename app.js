cdfe7c41b7868dafadc48a7697c7bb7163531fc6
macbookpro <macbookpro@macbookpros-MacBook-Pro.local>
2024-03-03 14:19:53 +0500

    ..

--- app.js
+++ app.js
@@ -2,48 +2,43 @@ const nodemailer = require('nodemailer');
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors({ origin: "*" }));
-app.use(express.json())
-// app.use(function (req, res, next) {
+const dotenv = require('dotenv');
+dotenv?.config();
+const allowedOrigins = ['http://localhost:4001', 'https://sss-g-server.vercel.app', 'https://api.fitwellhub.com'];
 
-//   // Website you wish to allow to connect
-//   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   // res.setHeader('Access-Control-Allow-Origin', 'http://glonetex.com/');
+// Configure CORS middleware to allow only specified origins
+app.use(cors({
+  origin: function (origin, callback) {
+    // Check if the request origin is in the list of allowed origins
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block the request
+    }
+  }
+}));app.use(express.json())
 
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-// Create a transporter object using the provided SMTP settings
 app.get('/', (req,res)=>{return res.json({msg:"I am working"})} );
-app.use('/glonetex/email', (req, res) => {
-  console.log( req.body)
+
+app.use('/fitwell/email', (req, res) => {
+  
   const transporter = nodemailer.createTransport({
-    service: 'SMTP',
-    host: 'smtp.hostinger.com',
-    port: 465,
+    service: process?.env?.SERVICE,
+    host: process?.env?.HOST,
+    port: process?.env?.PORT,
     secure: true, // Use SSL
     auth: {
-      user: 'no-replay@fitwellhub.com',
-      pass: 'No-Replay@password@1212',
+      user: process?.env?.USER,
+      pass: process?.env?.PASSWORD,
     },
   });
 
   // Define the email content
   const mailOptions = {
-    from: 'no-replay@fitwellhub.com', // Use your service's email address here
-    to: req.body.email,
-    subject: req.body.subject,
-    html: req.body.html,
+    from: process?.env?.USER, // Use your service's email address here
+    to: req?.body?.email,
+    subject: req?.body?.subject,
+    html: req?.body?.html,
   };
   // const mailOptions = {
   //   from: 'solution.sales@glonetex.com', // Use your service's email address here
@@ -52,10 +47,12 @@ app.use('/glonetex/email', (req, res) => {
   //   text: 'This is a test email sent from Nest JS using Nestmailer.',
   // };
   // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
+  
+  transporter?.sendMail(mailOptions, (error, info) => {
+    console.log("error====>", error)
+    console.log("info=====>", info)
     if (error) {
       return res.json({status:false, msg:error})
-
     } else {
       return res.json({status:true, msg:info.response})
    
